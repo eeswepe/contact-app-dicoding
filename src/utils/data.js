09ba0c67e5c2eb05cfabe14db2d9581b23304aa1
singgih wahyu permana <singgihwahyupermana@gmail.com>
2024-07-24 05:45:42 +0000

    make ready to deploy

--- src/utils/data.js
+++ src/utils/data.js
@@ -2,21 +2,9 @@ const getData = () => {
   return [
     {
       id: 1,
-      name: "Dimas Saputra",
-      tag: "dimasmds",
-      imageUrl: "/images/dimasmds.jpeg",
-    },
-    {
-      id: 2,
-      name: "Arif Faizin",
-      tag: "arifaizin",
-      imageUrl: "/images/arifaizin.jpeg",
-    },
-    {
-      id: 3,
-      name: "Rahmat Fajri",
-      tag: "rfajri27",
-      imageUrl: "/images/rfajri27.jpeg",
+      name: "Singgih Wahyu Permana",
+      tag: "eeswepe",
+      imageUrl: "/images/default.jpg",
     },
   ];
 };
