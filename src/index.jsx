9f44239359e79571b625cef7fcb5ed054b2ece37
singgih wahyu permana <singgihwahyupermana@gmail.com>
2024-07-24 03:18:33 +0000

    restore to first commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
-import ContactApp from "./Components/ContactApp.jsx"
+import ContactApp from "./components/ContactApp"
 
 import './styles/style.css'
 
