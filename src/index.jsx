a5acd4305102e204befe9e14be5adf4b2374814a
singgih wahyu permana <singgihwahyupermana@gmail.com>
2024-07-24 03:16:12 +0000

    fixing folder name "components" to "Components"

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
-import ContactApp from "./Components/ContactApp"
+import ContactApp from "./Components/ContactApp.jsx"
 
 import './styles/style.css'
 
